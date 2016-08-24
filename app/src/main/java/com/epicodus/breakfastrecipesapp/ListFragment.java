d1de6f1aaa6a12a2623c19f98b09e54688e10051
Kyle <kderrick1988@gmail.com>
2016-08-24 10:02:11 -0700

    add onListRecipeSelected interface to ListFragment class

--- app/src/main/java/com/epicodus/breakfastrecipesapp/ListFragment.java
+++ app/src/main/java/com/epicodus/breakfastrecipesapp/ListFragment.java
@@ -10,6 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ListFragment extends Fragment {
+
+    public interface onRecipeSelectedInterface {
+        void onListRecipeSelected(int index);
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
