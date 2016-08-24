8813bc79718fbaf0b5fcc0ad079e82457e9b9bb0
Kyle <kderrick1988@gmail.com>
2016-08-24 09:54:18 -0700

    add recyclerView to ListFragment

--- app/src/main/java/com/epicodus/breakfastrecipesapp/ListFragment.java
+++ app/src/main/java/com/epicodus/breakfastrecipesapp/ListFragment.java
@@ -3,6 +3,8 @@ package com.epicodus.breakfastrecipesapp;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,16 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
         return view;
     }
+
+
 }
