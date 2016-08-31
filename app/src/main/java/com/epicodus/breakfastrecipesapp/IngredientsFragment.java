3f292bc7404cd2bc6a100c38ebbae799cca22e79
Kyle <kderrick1988@gmail.com>
2016-08-31 13:16:57 -0700

    refactor to use a base class for DirectionsFragment and IngredientsFragment

---
+++ app/src/main/java/com/epicodus/breakfastrecipesapp/IngredientsFragment.java
@@ -0,0 +1,8 @@
+package com.epicodus.breakfastrecipesapp;
+
+public class IngredientsFragment extends CheckBoxesFragment {
+    @Override
+    public String[] getContents(int index) {
+        return Recipes.ingredients[index].split("`");
+    }
+}
